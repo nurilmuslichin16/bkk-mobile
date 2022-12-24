9817a7ec53910ce59f25a33ebfbfea17843a1d3f
Nuril Muslichin <nurilmuslichin16@gmail.com>
2022-12-24 07:59:20 +0700

    Build Release

--- lib/shared/variabel.dart
+++ lib/shared/variabel.dart
@@ -1,3 +1,9 @@
-String baseUrl = 'http://192.168.100.29/bkk-smk/api';
-String baseUrlImage = 'http://192.168.100.29/bkk-smk/assets/images';
-String baseUrlFotoPerusahaan = 'http://192.168.100.29/bkk-smk/upload/';
+// Localhost
+// String baseUrl = 'http://192.168.100.29/bkk-smk/api';
+// String baseUrlImage = 'http://192.168.100.29/bkk-smk/assets/images';
+// String baseUrlFotoPerusahaan = 'http://192.168.100.29/bkk-smk/upload/';
+
+// Hosting
+String baseUrl = 'http://inventaris-smkdwijapraja.my.id/api';
+String baseUrlImage = 'http://inventaris-smkdwijapraja.my.id/assets/images';
+String baseUrlFotoPerusahaan = 'http://inventaris-smkdwijapraja.my.id/upload/';
